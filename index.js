8775f7f3e9e67edea3886c88d61c92038022139c
MatthewJacquez <matt.cras@gmail.com>
2023-03-16 19:06:59 -0700

    commit tree 1480956d8a076b886ed96bad35d881c190b73a02

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// cats.push("Ralph");
+
+function destructivelyAppendCat(name) {
+   cats.push(name); 
+   //console.log("Matt",name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+} 
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+} 
+
+ function appendCat(name) {
+//     const copyOfCats = [...cats];
+//   copyOfCats.push(name);
+//   return copyOfCats;
+return [...cats, name];
+}
+
+function prependCat(name) {
+    return [ name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,cats.length -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1, cats.length);
+}
